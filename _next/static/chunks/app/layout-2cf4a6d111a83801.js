dae1e318136d607a7c6105ab3967e9417191dd1d
brodyKim <coffeescript@kakao.com>
2026-02-20 00:19:44 +0900

    main page apply i18n, modify layout & design

---
+++ _next/static/chunks/app/layout-2cf4a6d111a83801.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7498:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7638,23))},7638:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[665,102,337,358],()=>s(7498)),_N_E=e.O()}]);
